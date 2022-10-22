981c3251c618734f72ae7ffe899c45ac27e92b44
Nelson Arevalo <nelson.arevalo2119@gmail.com>
2022-10-22 01:40:44 -0500

    Merge errors

---
+++ src/app/views/components/input/input.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent implements OnInit {
+  /** Props */
+  @Input()
+  public label!: string;
+
+  @Input()
+  public type!: string;
+
+  @Input()
+  public name!: string;
+
+  /** Variables */
+  public isFocused: boolean = false;
+  public passwordText: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * This function set the isFocused variable for change the class from active to inactive.
+   * @return {void}
+   */
+  setIsFocus(): void {
+    this.isFocused = !this.isFocused;
+  }
+
+  /**
+   * This function return the text entered in the input password.
+   * @param {string} event - entered text.
+   * @return {void}
+   */
+   showInput(event: string | any): void {
+    this.passwordText = event;
+   }
+}
